8639063495c11bef7e9c287082b89f7a9070fde7
Akira TAGOH <akira@tagoh.org>
2010-12-24 18:22:36 +0900

    update testcase

--- tests/rhbz_526205.c
+++ tests/rhbz_526205.c
@@ -1,7 +1,7 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /* 
  * rhbz_526205.c
- * Copyright (C) 2009 Red Hat, Inc. All rights reserved.
+ * Copyright (C) 2009-2010 Red Hat, Inc. All rights reserved.
  * 
  * Authors:
  *   Akira TAGOH  <tagoh@redhat.com>
@@ -28,14 +28,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "imsettings/imsettings.h"
-#include "imsettings/imsettings-request.h"
-#include "imsettings/imsettings-info.h"
-#include "imsettings/imsettings-info-private.h"
+#include "imsettings.h"
+#include "imsettings-client.h"
+#include "imsettings-info.h"
 #include "main.h"
 
-DBusConnection *dbus_conn;
-IMSettingsRequest *req;
+IMSettingsClient *client;
 
 /************************************************************/
 /* common functions                                         */
@@ -43,8 +41,7 @@ IMSettingsRequest *req;
 void
 setup(void)
 {
-	dbus_conn = dbus_bus_get(DBUS_BUS_SESSION, NULL);
-	req = imsettings_request_new(dbus_conn, IMSETTINGS_INTERFACE_DBUS);
+	client = imsettings_client_new(NULL);
 }
 
 void
@@ -52,8 +49,7 @@ teardown(void)
 {
 	imsettings_test_reload_daemons();
 
-	g_object_unref(req);
-	dbus_connection_unref(dbus_conn);
+	g_object_unref(client);
 }
 
 /************************************************************/
@@ -70,10 +66,10 @@ TDEF (issue) {
 
 	g_usleep(5 * G_USEC_PER_SEC);
 
-	v = imsettings_request_get_current_user_im(req, &error);
+	v = imsettings_client_get_user_im(client, NULL, &error);
 	fail_unless(v != NULL, "Unexpected the user IM: (null)");
 	fail_unless(strcmp(v, IMSETTINGS_XIM_CONF) != 0, "Unexpected the user IM: %s", v);
-	info = imsettings_request_get_info_object(req, "X locale compose", &error);
+	info = imsettings_client_get_info_object(client, "X locale compose", NULL, &error);
 	fail_unless(info == NULL, "Unexpected object.");
 #if 0
 	info = imsettings_request_get_info_object(req, IMSETTINGS_XIM_CONF, &error);
@@ -82,11 +78,11 @@ TDEF (issue) {
 	fail_unless(strcmp(v, "none") == 0, "Unexpected the short description: %s", v);
 #endif
 
-	imsettings_request_set_locale(req, "pt_BR.UTF-8");
-	v = imsettings_request_get_current_user_im(req, &error);
+	imsettings_client_set_locale(client, "pt_BR.UTF-8");
+	v = imsettings_client_get_user_im(client, NULL, &error);
 	fail_unless(v != NULL, "Unexpected the user IM (take 2): (null)");
 	fail_unless(strcmp(v, IMSETTINGS_XIM_CONF) != 0, "Unexpected the user IM (take 2): %s", v);
-	info = imsettings_request_get_info_object(req, "X locale compose", &error);
+	info = imsettings_client_get_info_object(client, "X locale compose", NULL, &error);
 	fail_unless(info != NULL, "Unable to obtain IMSettingsInfo (take 2)");
 	v = imsettings_info_get_short_desc(info);
 	fail_unless(strcmp(v, "X locale compose") == 0, "Unexpected the short description (take 2): %s", v);
